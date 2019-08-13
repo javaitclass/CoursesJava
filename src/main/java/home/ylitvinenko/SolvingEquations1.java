4dcbab5da7b550710205971767e9cfdaa80173a2
ylitvinenko <dnepr88@gmail.com>
2019-08-13 14:21:19 +0300

    Homework 1.1 Solving Equations

--- src/main/java/home/ylitvinenko/SolvingEquations1.java
+++ src/main/java/home/ylitvinenko/SolvingEquations1.java
@@ -4,27 +4,27 @@ public class SolvingEquations1 {
     public static void main(String[] args) {
         System.out.println(solvingEquation1(10,20,30,50));
         System.out.println(solvingEquation2(10,20,30,50));
-        System.out.println(solvingEquation3(10,20,30,50));
-        System.out.println(solvingEquation4(10,20,30,50));
+        System.out.println(solvingEquation3(1,1,3,50));
+        System.out.println(solvingEquation4(10.11,20.22,30.22,50.11));
 
 
     }
-    public static long solvingEquation1 (int x,int y, int z, int n)
+    private static long solvingEquation1 (long x,long y, long z, long n)
     {
         long result1= x - ((y + 123) / z) * n;
         return result1;
     }
-    public static int solvingEquation2 (int x,int y, int z, int n)
+    private static int solvingEquation2 (int x,int y, int z, int n)
     {
         int result2= (x + (50 - y * z) / n) - x;
         return result2;
     }
-    public static float solvingEquation3 (int x,int y, int z, int n)
+    private static float solvingEquation3 (float x,float y, float z, float n)
     {
         float result3 = x / (y * (z - 1) - 564) + n;
         return result3;
     }
-    public static double solvingEquation4 (int x,int y, int z, int n)
+    private static double solvingEquation4 (double x,double y, double z, double n)
     {
         double result4 = -x / -y + z / (n + 15);
         return result4;
