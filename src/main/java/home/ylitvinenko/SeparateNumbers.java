3ed6b85fe309655686440dd71477e7ba60a4cf44
ylitvinenko <dnepr88@gmail.com>
2019-08-15 11:52:45 +0300

    separate numbers homework - fix

--- src/main/java/home/ylitvinenko/SeparateNumbers.java
+++ src/main/java/home/ylitvinenko/SeparateNumbers.java
@@ -2,18 +2,32 @@ package home.ylitvinenko;
 
 public class SeparateNumbers {
     public static void main(String[] args) {
-        int a = 95711;
-        int b = 10000,c = 1000,d = 100, e = 10;
-        int f = a/b;
-        int g = (a-b*f)/c;
-        int i = ((a-b*f)-(g*c))/d;
-        int j = (((a-b*f) - (g*c))-(i*d))/e;
-        int k = (((a-b*f) - (g*c))-(i*d))-(j*e);
-        System.out.println(f);
-        System.out.println(g);
-        System.out.println(i);
-        System.out.println(j);
-        System.out.println(k);
+       separate(97852);
 
+
+
+    }
+    public static int separate (int number)
+    {
+        int result = number/10000;
+        System.out.println(result);
+
+        number = number%10000;
+        result = number /1000;
+        System.out.println(result);
+
+        number = number%1000;
+        result = number /100;
+        System.out.println(result);
+
+        number = number%100;
+        result = number /10;
+        System.out.println(result);
+
+        number = number%10;
+        result = number;
+        System.out.println(result);
+
+        return result;
     }
 }
