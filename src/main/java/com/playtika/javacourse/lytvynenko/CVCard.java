f001dcf8ad079edede632ca6fefaafed90149e0a
ylitvinenko <dnepr88@gmail.com>
2019-08-20 09:16:12 +0300

    Homework 2.1 - CV Card - Fix

--- src/main/java/com/playtika/javacourse/lytvynenko/CVCard.java
+++ src/main/java/com/playtika/javacourse/lytvynenko/CVCard.java
@@ -6,13 +6,13 @@ public class CVCard {
     public static void drowStarsLine () {
         String fullName = new String("Lytvynenko Yuriy Nikolaevich");
         StringBuffer netwidth = new StringBuffer("");
-        for (int i = 0; i<fullName.length()+4;i++) {
+        for (int i = 0; i < fullName.length()+4;i++) {
             netwidth.append("*");
         }
         System.out.println(netwidth);
-        System.out.println((centerOfWords(netwidth,"Course: Java core")));
-        System.out.println((centerOfWords(netwidth,"Student")));
-        System.out.println((centerOfWords(netwidth,fullName)));
+        System.out.println((centerOfWords(netwidth, "Course: Java core")));
+        System.out.println((centerOfWords(netwidth, "Student")));
+        System.out.println((centerOfWords(netwidth, fullName)));
         System.out.println(netwidth);
     }
     public static StringBuffer centerOfWords(StringBuffer net, String wordsWriter) {
@@ -20,7 +20,7 @@ public class CVCard {
         int a = c / 2;
         int b = c - a ;
         StringBuffer result = new StringBuffer("*");
-        for (int i = 0; i<(a-1); i++) {
+        for (int i = 0; i < (a-1); i++) {
             result.append(" ");
         }
         result.append(wordsWriter);
