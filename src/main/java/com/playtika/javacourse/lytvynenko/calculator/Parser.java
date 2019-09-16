7c66704a6a960df42a8bec51e9cc706b436be9fc
ylitvinenko <dnepr88@gmail.com>
2019-09-16 14:27:30 +0300

    HomeWork 5 - added fixes

--- src/main/java/com/playtika/javacourse/lytvynenko/calculator/Parser.java
+++ src/main/java/com/playtika/javacourse/lytvynenko/calculator/Parser.java
@@ -1,27 +1,21 @@
 package com.playtika.javacourse.lytvynenko.calculator;
 
 public class Parser {
-
-    private String[] splitting (String enteredText){
-        String splittedStr [] = enteredText.split(" ");
-        return splittedStr;
+    public String[] splitting (String enteredText){
+        return enteredText.split(" ");
     }
 
     public double firstOperandParsing (String enteredText){
-        String number = splitting(enteredText)[0];
-         double x = Double.parseDouble(number);
-        return x;
+        enteredText = splitting(enteredText)[0];
+        return Double.parseDouble(enteredText);
     }
 
     public String operationParsing (String enteredText){
-        String operation = splitting(enteredText)[1];
-        return operation;
+        return splitting(enteredText)[1];
     }
 
     public double secondOperandParsing (String enteredText){
-        String number = splitting(enteredText)[2];
-        double y = Double.parseDouble(number);
-        return y;
+        enteredText = splitting(enteredText)[2];
+        return Double.parseDouble(enteredText);
     }
-
 }
